6311e60ddb6d4e2057c597686ae5c543f006c3a6
Fobri <Fobri@users.noreply.github.com>
2021-04-09 15:15:23 +0300

    Added directions for float3
    Can use float3.right etc like with Vector3 now

--- Assets/Voxelbased/Core/Voxel/Utils/MathExtensions.cs
+++ Assets/Voxelbased/Core/Voxel/Utils/MathExtensions.cs
@@ -7,9 +7,35 @@ namespace VoxelbasedCom.Extensions
 {
     public static class MathExtensions
     {
-        public static float magnitude(this float3 i)
+        public static float magnitude(this float3 x)
         {
-            return math.sqrt(math.pow(i.x, 2) + math.pow(i.y, 2) + math.pow(i.z, 2));
+            return math.sqrt(math.pow(x.x, 2) + math.pow(x.y, 2) + math.pow(x.z, 2));
+        }
+
+
+        public static float3 up(this float3 x)
+        {
+            return new float3(0, 1, 0);
+        }
+        public static float3 down(this float3 x)
+        {
+            return new float3(0, -1, 0);
+        }
+        public static float3 right(this float3 x)
+        {
+            return new float3(1, 0, 0);
+        }
+        public static float3 left(this float3 x)
+        {
+            return new float3(-1, 0, 0);
+        }
+        public static float3 forward(this float3 x)
+        {
+            return new float3(0, 0, 1);
+        }
+        public static float3 back(this float3 x)
+        {
+            return new float3(0, 0, -1);
         }
     }
 }
